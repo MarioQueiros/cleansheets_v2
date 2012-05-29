a5d12f4b90c73aac7d9a44e8261c64d9dcbbb1ab
Bruno_Cunha <Bruno_Cunha@localhost>
2012-05-29 17:01:57 +0100

    In?cio do trabalho de convers?o das f?rmulas existentes

--- src/csheets/core/formula/compiler/FormulaLexer.java
+++ src/csheets/core/formula/compiler/FormulaLexer.java
@@ -102,6 +102,12 @@ tryAgain:
 					theRetToken=_returnToken;
 					break;
 				}
+                               case'#':
+                                   {
+					mCard(true);
+					theRetToken=_returnToken;
+					break;
+				}
 				case '&':
 				{
 					mAMP(true);
@@ -545,6 +551,19 @@ tryAgain:
 		int _saveIndex;
 		
 		match("=");
+		if ( _createToken && _token==null && _ttype!=Token.SKIP ) {
+			_token = makeToken(_ttype);
+			_token.setText(new String(text.getBuffer(), _begin, text.length()-_begin));
+		}
+		_returnToken = _token;
+	}
+        
+        public final void mCard(boolean _createToken) throws RecognitionException, CharStreamException, TokenStreamException {
+		int _ttype; Token _token=null; int _begin=text.length();
+		_ttype = EQ;
+		int _saveIndex;
+		
+		match("#");
 		if ( _createToken && _token==null && _ttype!=Token.SKIP ) {
 			_token = makeToken(_ttype);
 			_token.setText(new String(text.getBuffer(), _begin, text.length()-_begin));
