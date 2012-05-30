1c4a6b8394a8e31eb0bab6f3716b8b1f15f86135
MpApQ <MpApQ@localhost>
2012-05-30 16:46:11 +0100

    XML Escrita+Leitura

--- src/csheets/io/XMLCodec.java
+++ src/csheets/io/XMLCodec.java
@@ -1,7 +1,10 @@
 package csheets.io;
 
+import csheets.core.Cell;
 import csheets.core.Spreadsheet;
 import csheets.core.Workbook;
+import csheets.ext.style.StylableSpreadsheet;
+import csheets.ext.style.StyleExtension;
 import java.io.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -12,6 +15,10 @@ import org.w3c.dom.*;
  * @author MpApQ
  */
 public class XMLCodec implements Codec {
+
+    String[] columns = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG",
+        "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ"};
 
     public XMLCodec() {
     }
@@ -28,7 +35,7 @@ public class XMLCodec implements Codec {
             doc.getDocumentElement().normalize();
             System.out.println("Root element " + doc.getDocumentElement().getNodeName());
             NodeList nodeLst = doc.getElementsByTagName("row");
-
+            String rowID = "", columnID = "", contentStr = "";
             for (int s = 0; s < nodeLst.getLength(); s++) {
 
                 Node fstNode = nodeLst.item(s);
@@ -37,36 +44,36 @@ public class XMLCodec implements Codec {
 
                     Element fstElmnt = (Element) fstNode;
                     NamedNodeMap attrs = fstNode.getAttributes();
-                    System.out.println("attrs.getLength()"+attrs.getLength());
                     for (int i = 0; i < attrs.getLength(); i++) {
                         Attr attribute = (Attr) attrs.item(i);
-                        System.out.println("Row"+attribute.getName() + "=" + attribute.getValue());
+                        rowID = attribute.getValue();
                     }
 
-                    
+
                     NodeList fstNmElmntLst = fstElmnt.getElementsByTagName("column");
+                    int l = 0;
+                    for (int i = 0; i < fstNmElmntLst.getLength(); i++) {
+                        Node fstNodeAtri = fstNmElmntLst.item(i);
+                        NamedNodeMap attrsAtr = fstNodeAtri.getAttributes();
+                        for (int j = 0; j < attrsAtr.getLength(); j++) {
+                            Attr attribute = (Attr) attrsAtr.item(j);
+                            columnID = attribute.getValue();
+
+                        }
+                        NodeList fstNmElmntLstCont = fstElmnt.getElementsByTagName("content");
 
 
-//                    for (int i = 0; i < fstNmElmntLst.getLength(); i++) {
-//                        Element fstNmElmnt = (Element) fstNmElmntLst.item(i);
-                    NodeList fstNmElmntLstCont = fstElmnt.getElementsByTagName("content");
-                    for (int i = 0; i < fstNmElmntLstCont.getLength(); i++) {
-                        Element fstNmElmnt = (Element) fstNmElmntLstCont.item(i);
+                        Element fstNmElmnt = (Element) fstNmElmntLstCont.item(l);
                         NodeList fstNm = fstNmElmnt.getChildNodes();
-                        System.out.println("RowID: ******* - ColumnID: ******* - Content: " + ((Node) fstNm.item(0)).getNodeValue());
+
+                        contentStr = ((Node) fstNm.item(l)).getNodeValue();
+                        l++;
+
+
+
+                        System.out.println("RowID: " + rowID + " - ColumnID: " + columnID + " - Content: " + contentStr);
                     }
 
-//                        NodeList fstNm = fstNmElmnt.getChildNodes();
-//                        System.out.println("First Name : "  + ((Node) fstNm.item(i)).getNodeValue());
-//                    }
-
-//                    Element fstNmElmnt = (Element) fstNmElmntLst.item(0);
-//                    NodeList fstNm = fstNmElmnt.getChildNodes();
-//                    System.out.println("First Name : " + ((Node) fstNm.item(0)).getNodeValue());
-//                    NodeList lstNmElmntLst = fstElmnt.getElementsByTagName("lastname");
-//                    Element lstNmElmnt = (Element) lstNmElmntLst.item(0);
-//                    NodeList lstNm = lstNmElmnt.getChildNodes();
-//                    System.out.println("Last Name : " + ((Node) lstNm.item(0)).getNodeValue());
                 }
             }
         } catch (Exception e) {
@@ -80,14 +87,22 @@ public class XMLCodec implements Codec {
     public void write(Workbook workbook, OutputStream stream) throws IOException {
         PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(stream)));
         Spreadsheet sheet = workbook.getSpreadsheet(0);
+        for (int i = 0; i < workbook.getSpreadsheet(0).getRowCount(); i++) {
+        }
+
+        //Cell[] cells = workbook.getSpreadsheet(0).getRow(0);
+        StylableSpreadsheet st = (StylableSpreadsheet) sheet.getExtension(StyleExtension.NAME);
+//        System.out.println("st.getColumnWidth(0): " + st.getColumnWidthXML(0));
+        System.out.println("st.getRowHeight(0): " + st.getRowHeight(0));
+
         writer.print("<?xml version='1.0' encoding='UTF-8'?>\n");
         writer.print("<cleanSheets>\n");
         for (int row = 0; row < sheet.getRowCount(); row++) {
-            writer.print("\t<row id='" + row + "'>\n");
+            writer.print("\t<row id='" + row + "' rowHeight='" + st.getRowHeight(row) + ">\n");
             for (int column = 0; column <= sheet.getColumnCount(); column++) {
                 String aux = sheet.getCell(column, row).getContent();
                 if (aux.trim() != "") {
-                    writer.print("\t\t<column id='" + column + "'>\n");
+                    writer.print("\t\t<column id='" + columns[column] + "' width='" + st.getColumnWidth(0) + "'\n");
                     writer.print("\t\t\t<content>" + sheet.getCell(column, row).getContent() + "</content>\n");
                     writer.print("\t\t</column>\n");
                 }
