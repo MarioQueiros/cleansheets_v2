8cecd77956d52869692d755c454c43434e27984b
tiagoqueiros <tiagoqueiros@localhost>
2012-05-30 10:45:05 +0100

    Adic?o dos menus do share page e seguinte ac??o destes

---
+++ src/csheets/ui/share/ClientAction.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csheets.ui.share;
+
+import csheets.ui.ctrl.BaseAction;
+import csheets.ui.share.ConnectFrame;
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author Tiago
+ */
+public class ClientAction extends BaseAction{
+    
+    ConnectFrame cFrame = new ConnectFrame();
+    
+    public ClientAction() {
+        cFrame.setVisible(false);
+    }
+    
+    @Override
+    protected String getName() {
+        return "Connect to...";
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        cFrame.setVisible(true);
+        cFrame.getIp();
+        cFrame.getFirstCell();
+        cFrame.getLastCell();
+    }
+    
+}
