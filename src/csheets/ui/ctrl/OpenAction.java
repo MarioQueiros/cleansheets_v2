309a8cbea91ad1b1f16092d6504bc2526e4546f8
MpApQ <MpApQ@localhost>
2012-06-01 11:23:09 +0100

    XML-Leitura-Escrita

--- src/csheets/ui/ctrl/OpenAction.java
+++ src/csheets/ui/ctrl/OpenAction.java
@@ -95,7 +95,6 @@ public class OpenAction extends BaseAction {
 					showErrorDialog("Primitive data was found in the input stream instead of objects.");
 				} catch (Exception e) {
 					showErrorDialog("An I/O error occurred when loading the file.");
-                                        e.printStackTrace();
 				}
 			else
 				uiController.setActiveWorkbook(workbook);
