6605d269789f39b8ba8291da2267313b4b14d2f1
joaodias <joaodias@joaodias-PC.dei.isep.ipp.pt>
2012-06-06 10:53:25 +0100

    Itera??o 2 completa

---
+++ src/csheets/bd/BaseDadosAddRefreshAction.java
@@ -0,0 +1,347 @@
+package csheets.bd;
+
+import java.awt.event.ActionEvent;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+import csheets.ui.ctrl.BaseAction;
+import csheets.ui.ctrl.UIController;
+
+public class BaseDadosAddRefreshAction extends BaseAction {
+
+    /** The user interface controller */
+    protected UIController uiController;
+    private int nrlinhas = 128, nrcolunas = 52;
+    private String[][] options = new String[nrlinhas][nrcolunas];
+    private String[][] matriz_sel;
+    private String[] temp = new String[2];
+    String[] coluna = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG",
+        "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ"};
+    private boolean flag_geral = false;
+    String infor[] = new String[5];
+    String pk[] = new String[52];
+    int[] linhas = new int[nrlinhas];
+    int[] vec = new int[4]; //Vector com os extremos seleccionados
+    String tipo_bd, nome_tabela;
+
+    public class ThreadBD implements Runnable {
+
+        public ThreadBD() {
+        }
+
+        @Override
+        public void run() {
+            reset();
+            escolhaSGBD();
+            escolherAreaCelulas();
+            preencherVectorCoord();
+            preencherMatrizFinal();
+            IBaseDados bd;
+
+            if (!flag_geral) {
+                bd = BaseDadosFactory.getBD(tipo_bd);
+                formulario(bd);
+            }
+        }
+    }
+
+    /**
+     * Creates a new font action.
+     * @param uiController the user interface controller
+     * 
+     */
+    public BaseDadosAddRefreshAction(UIController uiController) {
+        this.uiController = uiController;
+
+        //Colunas e linhas das sheets
+        for (int a = 0; a < nrlinhas; ++a) {
+            linhas[a] = a + 1;
+        }
+
+        //Preencher as opções possíveis tendo em conta o formato do Csheets
+        for (int i = 0; i < nrcolunas; ++i) {
+            for (int j = 0; j < nrlinhas; ++j) {
+                options[j][i] = coluna[i] + linhas[j];
+            }
+        }
+
+
+    }
+
+    public void reset() {
+        //flag começa sempre a false
+        flag_geral = false;
+
+
+    }
+
+    protected String getName() {
+        return "Actualizar/Adicionar";
+    }
+
+    protected void defineProperties() {
+    }
+
+    public boolean validaString(String a) {
+        for (int i = 0; i < nrlinhas; ++i) {
+            for (int j = 0; j < nrcolunas; ++j) {
+                if (options[i][j].equalsIgnoreCase(a)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+
+
+    }
+
+    public void escolherAreaCelulas() {
+        boolean flag = false;
+
+        do {
+            if (flag_geral) {
+                break;
+            }
+
+            if (flag) {
+                JOptionPane.showMessageDialog(null, "Introduziu valores errados ou no formato errado!");
+            }
+            flag = false;
+            String receive;
+            receive = JOptionPane.showInputDialog("Introduza a área das células que pretende gravar. Exemplo : A1-D5");
+
+
+            if (receive == null) {
+                flag_geral = true;
+                break;
+            }
+
+
+            //Dividir a String
+            if (!receive.contains("-")) {
+                flag = true;
+            }
+
+            if (receive.contains("-")) {
+                temp = receive.split("-");
+                if (!validaString(temp[0]) || !validaString(temp[1])) {
+                    flag = true;
+                }
+
+            }
+
+
+            if (receive.contains("-")) {
+                //Validar as posições do vector para os extremos seleccionados e preencher no vector vec
+                int[] valida = new int[4];
+                for (int i = 0; i < nrlinhas; ++i) {
+                    for (int j = 0; j < nrcolunas; ++j) {
+                        if (options[i][j].equalsIgnoreCase(temp[0])) {
+                            valida[0] = i;
+                            valida[1] = j;
+                        }
+                        if (options[i][j].equalsIgnoreCase(temp[1])) {
+                            valida[2] = i;
+                            valida[3] = j;
+                        }
+                    }
+                }
+                //Exemplo : Tem que ser A1-D5 e não D5-A1 || não pode ser A5-D1
+                if (valida[3] < valida[1] || valida[2] < valida[0]) {
+                    flag = true;
+                }
+
+            }
+
+
+
+
+        } while (!validaString(temp[0]) || !validaString(temp[1]) || flag);
+    }
+
+    public void preencherVectorCoord() {
+        //Procurar as posições do vector para os extremos seleccionados e preencher no vector vec
+
+        for (int i = 0; i < nrlinhas; ++i) {
+            for (int j = 0; j < nrcolunas; ++j) {
+                if (options[i][j].equalsIgnoreCase(temp[0])) {
+                    vec[0] = i;
+                    vec[1] = j;
+                }
+                if (options[i][j].equalsIgnoreCase(temp[1])) {
+                    vec[2] = i;
+                    vec[3] = j;
+                }
+            }
+        }
+
+    }
+
+    public void preencherMatrizFinal() {
+        // Matriz com a informação que o utilizador seleccionou
+        int nrlinhas2 = vec[2] - vec[0] + 1;
+        int nrcolunas2 = vec[3] - vec[1] + 1;
+        matriz_sel = new String[nrlinhas2][nrcolunas2];
+        int row = 0, col = 0;
+
+        for (int k = vec[0]; k <= vec[2]; k++) {
+
+            for (int l = vec[1]; l <= vec[3]; l++) {
+
+                matriz_sel[row][col] = this.uiController.getActiveSpreadsheet().getCell(l, k).getContent();
+                col++;
+
+            }
+            col = 0;
+            row++;
+        }
+    }
+
+    public void escolhaSGBD() {
+        //Qual o SGBD que pretende
+
+        boolean tipo_bd_f = false, tipobd_message = false;
+        do {
+
+            if (flag_geral) {
+                break;
+            }
+
+
+            if (tipobd_message) {
+                JOptionPane.showMessageDialog(null, "Esse sistema SGDB não está disponível, digite novamente!");
+            }
+
+            tipo_bd = JOptionPane.showInputDialog(null, "De que tipo de SGBD pretende carregar? (Postgres/MySQL/SQLserver)");
+            if (tipo_bd == null) {
+                flag_geral = true;
+                break;
+            }
+
+            if (tipo_bd.equalsIgnoreCase("mysql") || tipo_bd.equalsIgnoreCase("postgres") || tipo_bd.equalsIgnoreCase("sqlserver")) {
+                tipo_bd_f = true;
+            }
+            if (!tipo_bd_f) {
+                tipobd_message = true;
+            }
+        } while (!tipo_bd_f);
+
+    }
+
+    public void formulario(IBaseDados a) {
+        String dados_bd = "";
+        boolean flagbd = false, flagbd_message = false;
+        do {
+
+            if (flag_geral) {
+                break;
+            }
+
+            //Indicar o nome da base de dados destino e o nome da tabela
+            if (flagbd_message) {
+                JOptionPane.showMessageDialog(null, "Dados inválidos e/ou base de dados indisponível!");
+            }
+
+            dados_bd = JOptionPane.showInputDialog(null, "Indique base de dados destino, endereço ou ip de conexão, porta , user e pass no seguinte formato : \n"
+                    + "nomebd;conexao;porta;user;pass  (Se não há pass use o seguinte token no lugar de pass !#n0p@ss#!)");
+            if (dados_bd == null) {
+                flag_geral = true;
+                break;
+            }
+
+            infor = dados_bd.split(";");
+            if (infor.length == 5) {
+                flagbd = a.connectarbd(infor[0], tipo_bd, infor[1], infor[2], infor[3], infor[4]);
+            }
+            if (!flagbd) {
+                flagbd_message = true;
+            }
+        } while (!flagbd);
+
+
+
+
+        boolean flagtab = false, flagtab_message = false;
+        do {
+
+            if (flag_geral) {
+                break;
+            }
+            //Indicar o nome da base de dados destino e o nome da tabela
+            if (flagtab_message) {
+                JOptionPane.showMessageDialog(null, "Tabela não existente na base de dados!");
+            }
+            
+            nome_tabela = JOptionPane.showInputDialog(null, "Indique o nome da tabela:");
+            if (nome_tabela == null) {
+                flag_geral = true;
+                break;
+            }
+            int colunas = -100;
+            colunas = a.getNrColunas(nome_tabela, infor[0], tipo_bd, infor[1], infor[2], infor[3], infor[4]);
+            if (colunas == -1 || colunas == 0) {
+                flagtab_message = true;
+
+            }
+            if (colunas != matriz_sel[0].length && colunas != -1 && colunas != 0) {
+                JOptionPane.showMessageDialog(null, "Número de colunas incompatíveis com a tabela que pretende actualizar!\n Verifique a estrutura da informação!");
+                flag_geral = true;
+                break;
+            }
+            if (colunas == matriz_sel[0].length) {
+                flagtab = true;
+            }
+
+            if (nome_tabela == null) {
+                flag_geral = true;
+                break;
+            }
+
+
+        } while (!flagtab);
+        if (!flag_geral) {
+            int nrls = matriz_sel.length;
+            int aux = 0;
+            while (aux < nrls) {
+                String vector[] = new String[matriz_sel[0].length];
+                //Código sobre Inserts & etc aqui.
+                for (int i = 0; i < matriz_sel[0].length; i++) {
+                    vector[i] = matriz_sel[aux][i];
+                }
+                a.addNewInf(nome_tabela,infor[0], vector,  tipo_bd, infor[1], infor[2], infor[3], infor[4]);
+                aux++;
+            }
+            
+            JOptionPane.showMessageDialog(null, "Informação exportada com sucesso!");
+        }
+
+    }
+
+    /**
+     * Lets the user select a font from a chooser.
+     * Then applies the font to the selected cells in the focus owner table.
+     * @param event the event that was fired
+     */
+    public void actionPerformed(ActionEvent event) {
+
+        // Lets user select a font
+        //Perguntar qual a área da folha que o utilizador pretende gravar
+
+        // Vamos exemplificar como se acede ao modelo de dominio (o workbook)
+        try {
+            //Correr a thread criada
+            Thread a = new Thread(new ThreadBD());
+            a.start();
+
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            // para ja ignoramos a excepcao
+        }
+    }
+}
---
+++ src/csheets/bd/MySQL.java
@@ -0,0 +1,457 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csheets.bd;
+
+import csheets.ui.ctrl.UIController;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author joaodias
+ */
+public class MySQL implements IBaseDados {
+
+   Connection conn;
+    private String nome_tabela;
+    private String token_pass;
+    private String[] pk = new String[52];
+    private String[] vector_col;
+    String pk_final[];
+
+    //Verificar se existe alguma Coluna vazia
+    public boolean verificarErro(String[][] matriz) {
+        for (int i = 0; i < matriz[0].length; i++) {
+            if (matriz[0][i].equals("")) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean validarPK(String[] v, String[][] matriz_sele) {
+        
+        if(v.length==0)
+            return false;
+        
+        
+        //verificar se existem elementos repetidos
+        for (int k = 0; k < v.length-1; ++k) {
+            for (int l = k + 1; l < v.length; l++) {
+                if (v[k].equalsIgnoreCase(v[l])) {
+                    return false;
+                }
+            }
+        }
+        
+             
+        int nrcolunas = matriz_sele[0].length;
+        int num = -1;
+        for (int i = 0; i < v.length; i++) {
+            try {
+                num = Integer.parseInt(v[i]);
+            } catch (Exception E) {
+                return false;
+            }
+            if (num > nrcolunas || num <= 0) {
+                return false;
+            }
+
+        }
+
+        for (int j = 0; j < v.length; j++) {
+            pk[j] = matriz_sele[0][Integer.parseInt(v[j]) - 1];
+        }
+
+        return true;
+    }
+
+    public boolean connectarbd(String nome_bd, String tipobd, String end, String porta, String user, String pass) {
+        try {
+
+            verificarPass(pass);
+
+            String url = "";
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            url = "jdbc:mysql://" + end + ":" + porta + "/" + nome_bd;
+            conn = (Connection) DriverManager.getConnection(url, user, token_pass);
+            conn.setAutoCommit(false);
+            conn.close();
+            return true;
+        } catch (Exception e) {
+            //  e.printStackTrace();
+            return false;
+        }
+    }
+    //Cria as colunas
+
+    public String criarColunas(String[][] matriz) {
+        String coluna = "";
+        for (int i = 0; i < matriz[0].length; i++) {
+            if (i != 0 && i < matriz[0].length) {
+                coluna = coluna + ", ";
+            }
+            coluna = coluna + matriz[0][i] + " varchar(120)";
+
+        }
+        //PKs
+        coluna = coluna + ", ";
+        for (int j = 0; j < pk.length; j++) {
+            if (pk[j] != null) {
+                if (j < pk.length) {
+                }
+                if (j == 0) {
+                    coluna = coluna + "primary key (" + pk[j];
+                }
+                if (j > 0) {
+                    coluna = coluna + " ," + pk[j];
+                }
+            }
+        }
+        coluna = coluna + ")";
+        return coluna;
+
+    }
+
+    public void preencherInformacao(String[][] matriz) throws SQLException {
+
+        Statement st = (Statement) conn.createStatement();
+        for (int i = 0; i < matriz.length; i++) {
+            String infor = "insert into " + nome_tabela + " values ( ";
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (j != 0 && j < matriz[0].length) {
+                    infor = infor + ", ";
+                }
+                infor = infor + "'" + matriz[i][j] + "' ";
+            }
+            infor = infor + ", NULL );";
+            st.executeUpdate(infor);
+        }
+
+
+
+    }
+
+    public void verificarPass(String pass) {
+        if (pass.equalsIgnoreCase("!#n0p@ss#!")) {
+            token_pass = "";
+        } else {
+            token_pass = pass;
+        }
+    }
+
+    public boolean criarTabela(String nome_tab, String nome_bd, String[][] matriz, String tipobd, String end, String porta, String user, String pass) {
+
+        try {
+            nome_tabela = nome_tab;
+
+            verificarPass(pass);
+
+            String url = "";
+
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            url = "jdbc:mysql://" + end + ":" + porta + "/" + nome_bd;
+
+
+            conn = (Connection) DriverManager.getConnection(
+                    url, user, token_pass);
+            conn.setAutoCommit(false);
+            Statement st = (Statement) conn.createStatement();
+            String colunas = criarColunas(matriz);
+            String total = "create table " + nome_tabela + " (" + colunas + " , id_t_c int auto_increment not null, key(id_t_c));";
+            st.executeUpdate(total);
+            preencherInformacao(matriz);
+            st.close();
+            conn.commit();
+            conn.close();
+            return true;
+        } catch (Exception e) {
+             e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean carregarInformacao(String nome_tab, UIController ui, String nome_bd, String tipobd, String end, String porta, String user, String pass) {
+        try {
+            nome_tabela = nome_tab;
+
+            verificarPass(pass);
+
+            String url = "";
+
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            url = "jdbc:mysql://" + end + ":" + porta + "/" + nome_bd;
+
+
+            conn = (Connection) DriverManager.getConnection(
+                    url, user, token_pass);
+            conn.setAutoCommit(false);
+            Statement st = (Statement) conn.createStatement();
+            String query = "select * from " + nome_tab+" order by id_t_c";
+            String query_conta_col = "SELECT count(*) FROM information_schema.columns WHERE table_name = '" + nome_tab + "'";
+            ResultSet rs = st.executeQuery(query_conta_col);
+            int nrcolunas = 0;
+            //Ir buscar o número de colunas que a tabela tem
+            while (rs.next()) {
+                nrcolunas = rs.getInt(1);
+            }
+
+            rs = st.executeQuery(query);
+            int row = 0;
+            while (rs.next()) {
+                for (int i = 1; i <= nrcolunas-1; i++) {
+                    ui.getActiveSpreadsheet().getCell(i - 1, row).setContent(rs.getString(i));
+                }
+                row++;
+            }
+            st.close();
+            conn.commit();
+            conn.close();
+            return true;
+        } catch (Exception e) {
+            // e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+    @Override
+    public int getNrColunas(String nome_tab, String nome_bd, String tipobd, String end, String porta, String user, String pass) {
+        try {
+            nome_tabela = nome_tab;
+
+            verificarPass(pass);
+
+            String url = "";
+
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            url = "jdbc:mysql://" + end + ":" + porta + "/" + nome_bd;
+
+
+            conn = (Connection) DriverManager.getConnection(
+                    url, user, token_pass);
+            conn.setAutoCommit(false);
+            Statement st = (Statement) conn.createStatement();
+     
+            String query_conta_col = "SELECT count(*) FROM information_schema.columns WHERE table_name = '" + nome_tab + "'";
+            ResultSet rs = st.executeQuery(query_conta_col);
+            int nrcolunas = 0;
+            //Ir buscar o número de colunas que a tabela tem
+            while (rs.next()) {
+                nrcolunas = rs.getInt(1);
+            }
+            conn.commit();
+            conn.close();
+            return nrcolunas-1;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public String[] getNomeColunas(String nome_tab, String nome_bd, String tipobd, String end, String porta, String user, String pass) throws SQLException {
+        String[] nome_colunas;
+        // Buscar nome das colunas
+        String sql_col = "SHOW COLUMNS FROM "+nome_tab+" FROM "+nome_bd;
+        String url = "";
+
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        url = "jdbc:mysql://" + end + ":" + porta + "/" + nome_bd;
+
+        int j = 0;
+        j = getNrColunas(nome_tab, nome_bd, tipobd, end, porta, user, pass);
+        nome_colunas = new String[j];
+        conn = (Connection) DriverManager.getConnection(
+                url, user, token_pass);
+        conn.setAutoCommit(false);
+        Statement st = (Statement) conn.createStatement();
+        
+        ResultSet rs_1 = st.executeQuery(sql_col);
+       
+        
+        
+        
+
+        int k = 1;
+        while (rs_1.next()) {
+                
+          
+                nome_colunas[k-1] = rs_1.getString(1);
+                k++;
+            if(k>j)
+                break;
+        }
+         conn.commit();
+        conn.close();
+        
+        return nome_colunas;
+    }
+    
+    
+    public String[] getPKString(String nome_tab, String nome_bd, String tipobd, String end, String porta, String user, String pass) throws SQLException {
+        nome_tabela = nome_tab;
+        String pks[];
+        
+        String query="SELECT k.column_name FROM information_schema.table_constraints t JOIN information_schema.key_column_usage k USING ( constraint_name, table_schema, table_name ) WHERE t.constraint_type =  'PRIMARY KEY' AND t.table_schema =  '"+nome_bd+"' AND t.table_name =  '"+nome_tab+"' LIMIT 0 , 30";
+         String url = "";
+
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            url = "jdbc:mysql://" + end + ":" + porta + "/" + nome_bd;
+
+
+            conn = (Connection) DriverManager.getConnection(
+                    url, user, token_pass);
+            conn.setAutoCommit(false);
+            Statement st = (Statement) conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            int nr_pk=0;
+            while(rs.next()){
+                nr_pk++;
+            }
+            pks= new String[nr_pk];
+            ResultSet rs2 = st.executeQuery(query);
+            int j=0;
+            while(rs2.next()){
+               pks[j]=rs2.getString(1);
+               j++;
+            }
+            st.close();
+            conn.commit();
+            conn.close();
+            
+            return pks;
+    
+    }
+    
+      public int getPosCol(String col,String nome_tab,String nome_bd, String tipobd, String end, String porta, String user, String pass) throws SQLException{
+        String[] vec=  getNomeColunas(nome_tab, nome_bd, tipobd, end, porta, user, pass);
+         for(int i=0;i<vec.length;i++){
+             if(col.equalsIgnoreCase(vec[i]))
+                    return i;
+         }
+             return -1;
+      
+    }
+
+    public boolean addNewInf(String nome_tab, String nome_bd, String[] v, String tipobd, String end, String porta, String user, String pass) {
+        nome_tabela = nome_tab;
+        String nome_colunas[];
+
+        String infor = "insert into " + nome_tabela + " values ( ";
+        for (int i = 0; i < v.length; i++) {
+            if (i < v.length - 1) {
+                infor += "'" + v[i] + "' " + ",";
+            }
+            if (i == v.length - 1) {
+                infor += "'" + v[i] + "' " + " , NULL);";
+            }
+
+        }
+
+        try {
+            String url = "";
+
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            url = "jdbc:mysql://" + end + ":" + porta + "/" + nome_bd;
+
+
+            conn = (Connection) DriverManager.getConnection(
+                    url, user, token_pass);
+            conn.setAutoCommit(false);
+            Statement st = (Statement) conn.createStatement();
+            
+            st.executeUpdate(infor);
+            st.close();
+            conn.commit();
+            conn.close();
+            return true;
+        } catch (Exception e) {
+            
+           
+
+            try {
+                conn.commit();
+                conn.close();
+                vector_col = getNomeColunas(nome_tab, nome_bd, tipobd, end, porta, user, pass);
+               
+            } catch (SQLException ex) {
+                
+                ex.printStackTrace();
+            }
+            try {
+                pk_final=getPKString(nome_tab, nome_bd, tipobd, end, porta, user, pass);
+            } catch (SQLException ex) {
+                Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+           
+            //Código UPDATE
+            String update = "update " + nome_tabela + " SET  ";
+            String up = "";
+            for (int i = 0; i < v.length; i++) {
+                if (i < v.length - 1) {
+                    up += vector_col[i] + "=" + "'" + v[i] + "' " + ",";
+                }
+                if (i == v.length - 1) {
+                    up += vector_col[i] + "=" + "'" + v[i] + "' ";
+                }
+
+
+            }
+            update = update + up;
+            update = update + " WHERE ";
+            for (int i = 0; i < pk_final.length; i++) {
+                if (i < pk_final.length - 1) {
+                    try {
+                        update += pk_final[i] + "=" + "'" + v[getPosCol(pk_final[i],nome_tab, nome_bd, tipobd, end, porta, user, pass)] + "' " + "AND ";
+                    } catch (SQLException ex) {
+                        Logger.getLogger(SQLserver.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                if (i == pk_final.length - 1) {
+                    try {
+                        update += pk_final[i] + "=" + "'" + v[getPosCol(pk_final[i],nome_tab, nome_bd, tipobd, end, porta, user, pass)]+ "' ";
+                    } catch (SQLException ex) {
+                        Logger.getLogger(SQLserver.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+
+            }
+
+
+           Statement ft;
+            try {
+               
+            String url = "";
+
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            url = "jdbc:mysql://" + end + ":" + porta + "/" + nome_bd;
+        
+                
+            conn = (Connection) DriverManager.getConnection(
+                    url, user, token_pass);
+            conn.setAutoCommit(false);
+            Statement st = (Statement) conn.createStatement();
+            
+            st.execute(update);
+            st.close();
+            conn.commit();
+            conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            
+
+            //e.printStackTrace();
+            return false;
+        }
+
+    }
+    
+}
