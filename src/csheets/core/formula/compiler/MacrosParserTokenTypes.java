ecd078b246582241f7ea6f46be3bb2bfa4322107
Hugo Dias <Hugo Dias@HugoDias-PC.dei.isep.ipp.pt>
2012-06-11 09:07:57 +0100

    hugo 99

---
+++ src/csheets/core/formula/compiler/MacrosParserTokenTypes.java
@@ -0,0 +1,37 @@
+// $ANTLR 2.7.5 (20050128): "../src/csheets/core/formula/compiler/MacrosCompiler.g" -> "MacrosLexer.java"$
+package csheets.core.formula.compiler;
+public interface MacrosParserTokenTypes {
+	int EOF = 1;
+	int NULL_TREE_LOOKAHEAD = 3;
+	int COLON = 4;
+	int EQ = 5;
+	int LOCVAR = 6;
+	int CELL_REF = 7;
+	int NEQ = 8;
+	int GT = 9;
+	int LT = 10;
+	int LTEQ = 11;
+	int GTEQ = 12;
+	int AMP = 13;
+	int PLUS = 14;
+	int MINUS = 15;
+	int MULTI = 16;
+	int DIV = 17;
+	int POWER = 18;
+	int PERCENT = 19;
+	int LPAR = 20;
+	int RPAR = 21;
+	int FUNCTION = 22;
+	int SEMI = 23;
+	int NAME = 24;
+	int NUMBER = 25;
+	int STRING = 26;
+	int LETTER = 27;
+	int ALPHABETICAL = 28;
+	int QUOT = 29;
+	int DIGIT = 30;
+	int ABS = 31;
+	int EXCL = 32;
+	int COMMA = 33;
+	int WS = 34;
+}
