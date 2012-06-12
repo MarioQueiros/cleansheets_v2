5655b633903736b9539a438640a19228eb7c8c75
Bruno <Bruno@Bruno-Portatil.dei.isep.ipp.pt>
2012-06-12 16:12:35 +0100

    CORE - IT 3 al?nea b) (falta aplicar thread e limpar lista de vars)

--- src/csheets/core/formula/compiler/RegistoVariaveis.java
+++ src/csheets/core/formula/compiler/RegistoVariaveis.java
@@ -1,7 +1,9 @@
 package csheets.core.formula.compiler;
 
+import csheets.core.Value;
 import csheets.core.formula.util.Variavel;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
@@ -10,23 +12,76 @@ import java.util.ArrayList;
 class RegistoVariaveis {
 
     ArrayList<Variavel> lista = null;
-    private static final RegistoVariaveis instance = new RegistoVariaveis();
 
-    private RegistoVariaveis() {
-        lista = new ArrayList<>();
-    }
-
-    public static RegistoVariaveis getInstance() {
-        return instance;
+    public RegistoVariaveis() {
+        lista = new ArrayList<Variavel>();
     }
 
     void add(String ref) {
         Variavel v = new Variavel(ref.replace("$", ""));
         lista.add(v);
     }
+
+    String getText(String func) {       //$temp5
+        String nome = func.replace("$", func);
+        for (Iterator<Variavel> it = lista.iterator(); it.hasNext();) {
+            Variavel variavel = it.next();
+            if (variavel.getId().equalsIgnoreCase(nome)) {
+                return variavel.getText();
+            }
+        }
+        return null;
+    }
+
+    void add(String ref, String txt, Value vl) {
+         String nome = ref.substring(1, ref.length());
+        Variavel v = new Variavel(nome, txt, vl);
+        if (hasVar(v)) 
+            sub(v);
+        else
+            lista.add(v);
+    }
+
+    private boolean hasVar(Variavel v) {
+        String nome = v.getId();
+        for (Iterator<Variavel> it = lista.iterator(); it.hasNext();) {
+            Variavel variavel = it.next();
+            if (variavel.getId().equalsIgnoreCase(nome)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Value getValue(String func) {  //func = $temp5
+        if(func.charAt(0)!=Variavel.VARIAVEL_STARTER)
+            return new Value("falta o caracter inicial");   
+        String nome = func.substring(1, func.length());
+        return getVarById(nome).getValor();
+       
+    }
+
+    private Variavel getVarById(String nome) {        
+        for (Iterator<Variavel> it = lista.iterator(); it.hasNext();) {
+            Variavel variavel = it.next();
+            if (variavel.getId().equalsIgnoreCase(nome)) 
+                return variavel;
+        }
+        return new Variavel("err", "VARIAVEL NAO EXISTE", new Value("VARIAVEL NAO EXISTE"));
+    }
+
+    private void sub(Variavel v) {
+        String nome = v.getId();
+        for (Iterator<Variavel> it = lista.iterator(); it.hasNext();) {
+            Variavel variavel = it.next();
+            if (variavel.getId().equalsIgnoreCase(nome)) {
+                variavel.clone(v);
+                return;
+            }
+        }
+        
+    }
 }
-
-
 // alterada para o formula.util
 /*
 public class Variavel {
