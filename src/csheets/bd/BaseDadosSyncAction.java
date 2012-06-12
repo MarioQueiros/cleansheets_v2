8d7e388f08d8439aac27c477669c578a661fb7d5
joaodias <joaodias@joaodias-PC>
2012-06-12 03:36:03 +0100

    Apagar e inserir na BD -> Apresentar e Remover da CS implementado

--- src/csheets/bd/BaseDadosSyncAction.java
+++ src/csheets/bd/BaseDadosSyncAction.java
@@ -1,5 +1,6 @@
 package csheets.bd;
 
+import csheets.core.formula.compiler.FormulaCompilationException;
 import java.awt.event.ActionEvent;
 
 import java.sql.SQLException;
@@ -11,6 +12,7 @@ import csheets.ui.ctrl.BaseAction;
 import csheets.ui.ctrl.UIController;
 
 public class BaseDadosSyncAction extends BaseAction {
+
     /** The user interface controller */
     protected UIController uiController;
     private int nrlinhas = 128, nrcolunas = 52;
@@ -24,12 +26,11 @@ public class BaseDadosSyncAction extends BaseAction {
     String infor[] = new String[5];
     String pk[] = new String[52];
     int[] linhas = new int[nrlinhas];
-    String [][]  matriz_bd , matriz_inicial;
-    
-   
+    String[][] matriz_bd, matriz_inicial;
     int valida_colunas;
     int[] vec = new int[4]; //Vector com os extremos seleccionados
     String tipo_bd, nome_tabela;
+
     public class ThreadBD implements Runnable {
 
         public ThreadBD() {
@@ -40,30 +41,54 @@ public class BaseDadosSyncAction extends BaseAction {
             reset();
             escolhaSGBD();
             IBaseDados bd;
-            if(!flag_geral){
-            bd = BaseDadosFactory.getBD(tipo_bd);
-            formulario(bd);
-            escolherAreaCelulas(bd);
-            preencherVectorCoord();
-            preencherMatrizFinal();
-            matriz_bd = bd.getMatrizInf(nome_tabela,infor[0] , tipo_bd, infor[1], infor[2], infor[3], infor[4]);
-            matriz_inicial= bd.getMatrizInf(nome_tabela,infor[0] , tipo_bd, infor[1], infor[2], infor[3], infor[4]);
-             SyncSingleton.Instance().setStop(false);
-            int i =0;
-            //Ciclo para a thread, de X em X segundos actualiza a informação.
-            while(!SyncSingleton.Instance().getStop()){
-                    try {
-                        Thread.currentThread().sleep(3000);
-                        getConteudoActual();
-                        System.out.println(i);
-                        i++;
-                        
-                        
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(BaseDadosSyncAction.class.getName()).log(Level.SEVERE, null, ex);
+            if (!flag_geral) {
+                bd = BaseDadosFactory.getBD(tipo_bd);
+                formulario(bd);
+                escolherAreaCelulas(bd);
+                preencherVectorCoord();
+                preencherMatrizFinal();
+                if (!flag_geral) {
+
+                    matriz_bd = bd.getMatrizInf(nome_tabela, infor[0], tipo_bd, infor[1], infor[2], infor[3], infor[4]);
+                    matriz_inicial = bd.getMatrizInf(nome_tabela, infor[0], tipo_bd, infor[1], infor[2], infor[3], infor[4]);
+                    SyncSingleton.Instance().setStop(false);
+                    int i = 0;
+
+
+                    //Ciclo para a thread, de X em X segundos actualiza a informação.
+                    while (!SyncSingleton.Instance().getStop()) {
+                        try {
+                            Thread.currentThread().sleep(5000);
+                            if (SyncSingleton.Instance().getStop()) {
+                                break;
+                            }
+                            //MATRIZ DA TABELA CLEANSHEETS
+                            getConteudoActual();
+                            //MATRIZ DA TABELA BD
+                            matriz_bd = bd.getMatrizInf(nome_tabela, infor[0], tipo_bd, infor[1], infor[2], infor[3], infor[4]);
+
+                            //COMPARA A MATRIZ_BD com a matriz inicial
+                            comparaMatrizBd_1(matriz_inicial, matriz_bd);
+                            comparaMatrizBd_2(matriz_inicial, matriz_bd);
+                            
+                            //COMPARA A MATRIZ CLEANSHEETS com a matriz inicial
+                            comparaMatrizCs_1(matriz_inicial, matriz_sel);
+                            comparaMatrizCs_2(matriz_inicial, matriz_sel);
+
+
+
+                            //Setup da Matriz Inicial
+                            saveMatrizActual(bd);
+                            System.out.println(i);
+                            i++;
+
+
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(BaseDadosSyncAction.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }
-            }
-            
+
+                }
             }
         }
     }
@@ -76,8 +101,8 @@ public class BaseDadosSyncAction extends BaseAction {
     public BaseDadosSyncAction(UIController uiController) {
         this.uiController = uiController;
 
-        
-       
+
+
         //Colunas e linhas das sheets
         for (int a = 0; a < nrlinhas; ++a) {
             linhas[a] = a + 1;
@@ -95,21 +120,192 @@ public class BaseDadosSyncAction extends BaseAction {
     public void reset() {
         //flag começa sempre a false
         flag_geral = false;
-        
+
 
     }
-    // Método para teste
-    public void printmatriz(){
-        for(int i=0;i<matriz_sel.length;i++){
-            for(int k=0;k<matriz_sel[0].length;k++){
-                System.out.println(matriz_sel[i][k]+" | ");
-            }
-            
-        }
+    
+    public void  saveMatrizActual(IBaseDados bd){
+        matriz_inicial=bd.getMatrizInf(nome_tabela, infor[0], tipo_bd, infor[1], infor[2], infor[3], infor[4]);
         
     }
+    
+    
+    /* Compara a matriz BD com a matriz anterior - removido bd -> remove células
+     * ou seja, a matriz inicial será sempre maior que a BD caso algum registo tenha
+     * sido eliminado da BD.
+     * 
+     */
+
+    public void comparaMatrizBd_1(String[][] m1, String[][] m2) {
+        String linha = "";
+        String linha_i = "";
+        for (int i = 0; i < m1.length; i++) {
+            linha = "";
+            for (int k = 0; k < m1[0].length; k++) {
+                linha += m1[i][k];
+
+            }
+            boolean flag = false;
+
+            for (int j = 0; j < m2.length; j++) {
+                linha_i = "";
+                for (int z = 0; z < m2[0].length; z++) {
+                    linha_i += m2[j][z];
+                }
+                //Encontrou o registo que foi apagado da BD
+                if (linha.equals(linha_i)) {
+                    flag = true;
+                }
+            }
+            //Tratar do registo
+            if (!flag) {
+                removeCelula(linha);
+            }
+        }
+    }
+
+    // Compara a matriz BD com a matriz anterior - inserido bd -> insere células
+    public void comparaMatrizBd_2(String[][] m1, String[][] m2) {
+        String linha = "";
+        String linha_i = "";
+        for (int i = 0; i < m2.length; i++) {
+            linha = "";
+            for (int k = 0; k < m2[0].length; k++) {
+                linha += m2[i][k];
+
+            }
+            boolean flag = false;
+
+            for (int j = 0; j < m1.length; j++) {
+                linha_i = "";
+                for (int z = 0; z < m1[0].length; z++) {
+                    linha_i += m1[j][z];
+                }
+                //Encontrou o registo 
+                if (linha.equals(linha_i)) {
+                    flag = true;
+
+                }
+
+            }
+
+
+            //Trata de inserir nas células
+            if (!flag) {
+                
+                linha = "";
+                for (int uu = 0; uu < m2[0].length; uu++) {
+
+                    linha = linha + m2[i][uu] + ";";
+
+                }
+
+                String vec[] = linha.split(";");
+              //System.out.println(linha);
+                insereCelula(vec);
+            }
+        }
+    }
+    
+    
+    // Compara a matriz CS com a matriz anterior - apaga na cs -> apaga BD
+    
+     public void comparaMatrizCs_1(String[][] m1, String[][] m2){
+         
+     }
+    
+     // Compara a matriz CS com a matriz anterior - insere na cs -> insere na BD
+     
+     public void comparaMatrizCs_2(String[][] m1, String[][] m2){
+         
+     }
+
+    //Insere na Célula
+    public void insereCelula(String[] s) {
+        String test = "";
+        int row = 0, col = 0;
+        boolean found=false;
+
+        for (int k = vec[0]; k <= vec[2]; k++) {
+
+            for (int l = vec[1]; l <= vec[3]; l++) {
+
+                test += this.uiController.getActiveSpreadsheet().getCell(l, k).getContent();
+                col++;
+
+            }
+
+            if (test.equals("")) {
+                found=true;
+                int xx = 0;
+                for (int kk = vec[1]; kk <= vec[3]; kk++) {
+
+                    try {
+                        this.uiController.getActiveSpreadsheet().getCell(kk, k).setContent(s[xx]);
+                        xx++;
+                    } catch (FormulaCompilationException ex) {
+                        Logger.getLogger(BaseDadosSyncAction.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                break;
+            }
+
+
+            test = "";
+            col = 0;
+            row++;
+        }
+        // Se não dá para preencher mais, então cancela-se a sincronização
+        if(!found){
+            JOptionPane.showMessageDialog(null,"O espaço que escolheu para as células, não aguenta mais conteúdo, sincronização cancelada!");
+            SyncSingleton.Instance().setStop(true);
+        }
+    }
+
+    //Remove da Célula
+    public void removeCelula(String s) {
+        String test = "";
+        int row = 0, col = 0;
+
+        for (int k = vec[0]; k <= vec[2]; k++) {
+
+            for (int l = vec[1]; l <= vec[3]; l++) {
+
+                test += this.uiController.getActiveSpreadsheet().getCell(l, k).getContent();
+                col++;
+
+            }
+
+            if (test.equals(s)) {
+                for (int kk = vec[1]; kk <= vec[3]; kk++) {
+                    try {
+                        this.uiController.getActiveSpreadsheet().getCell(kk, k).setContent("");
+                    } catch (FormulaCompilationException ex) {
+                        Logger.getLogger(BaseDadosSyncAction.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+
+
+            test = "";
+            col = 0;
+            row++;
+        }
+    }
+    // Método para teste
+
+    public void printmatriz() {
+        for (int i = 0; i < matriz_sel.length; i++) {
+            for (int k = 0; k < matriz_sel[0].length; k++) {
+                System.out.println(matriz_sel[i][k] + " | ");
+            }
+
+        }
+
+    }
     // Refresh à matriz da área que o utilizador decidiu sincronizar
-    public void getConteudoActual(){
+
+    public void getConteudoActual() {
         int row = 0, col = 0;
 
         for (int k = vec[0]; k <= vec[2]; k++) {
@@ -131,8 +327,7 @@ public class BaseDadosSyncAction extends BaseAction {
 
     protected void defineProperties() {
     }
-  
-    
+
     public boolean validaString(String a) {
         for (int i = 0; i < nrlinhas; ++i) {
             for (int j = 0; j < nrcolunas; ++j) {
@@ -145,14 +340,15 @@ public class BaseDadosSyncAction extends BaseAction {
 
 
     }
-   public void escolherAreaCelulas(IBaseDados a) {
+
+    public void escolherAreaCelulas(IBaseDados a) {
         boolean flag = false;
 
         do {
-               
-           valida_colunas = a.getNrColunas(nome_tabela, infor[0], tipo_bd, infor[1], infor[2], infor[3], infor[4]);
-            
-            
+
+            valida_colunas = a.getNrColunas(nome_tabela, infor[0], tipo_bd, infor[1], infor[2], infor[3], infor[4]);
+
+
             if (flag_geral) {
                 break;
             }
@@ -250,15 +446,15 @@ public class BaseDadosSyncAction extends BaseAction {
             row++;
         }
         // Verifica se a área que pretende sincronizar tem o mesmo número de colunas que a tabela
-        if(matriz_sel[0].length!=valida_colunas){
+        if (matriz_sel[0].length != valida_colunas) {
             JOptionPane.showMessageDialog(null, "Erro! Número de colunas incompatível");
-              flag_geral = true;
-             
+            flag_geral = true;
+
         }
-        
+
     }
 
-   public void escolhaSGBD() {
+    public void escolhaSGBD() {
         //Qual o SGBD que pretende
 
         boolean tipo_bd_f = false, tipobd_message = false;
@@ -290,76 +486,76 @@ public class BaseDadosSyncAction extends BaseAction {
     }
 
     public void formulario(IBaseDados a) {
-        
 
 
 
-            String dados_bd = "";
-            boolean flagbd = false, flagbd_message = false;
-            do {
 
-                if (flag_geral) {
-                    break;
-                }
+        String dados_bd = "";
+        boolean flagbd = false, flagbd_message = false;
+        do {
 
-                //Indicar o nome da base de dados destino e o nome da tabela
-                if (flagbd_message) {
-                    JOptionPane.showMessageDialog(null, "Dados inválidos e/ou base de dados indisponível!");
-                }
+            if (flag_geral) {
+                break;
+            }
 
-                dados_bd = JOptionPane.showInputDialog(null, "Indique base de dados destino, endereço ou ip de conexão, porta , user e pass no seguinte formato : \n"
-                        + "nomebd;conexao;porta;user;pass  (Se não há pass use o seguinte token no lugar de pass !#n0p@ss#!)");
-                if (dados_bd == null) {
+            //Indicar o nome da base de dados destino e o nome da tabela
+            if (flagbd_message) {
+                JOptionPane.showMessageDialog(null, "Dados inválidos e/ou base de dados indisponível!");
+            }
+
+            dados_bd = JOptionPane.showInputDialog(null, "Indique base de dados destino, endereço ou ip de conexão, porta , user e pass no seguinte formato : \n"
+                    + "nomebd;conexao;porta;user;pass  (Se não há pass use o seguinte token no lugar de pass !#n0p@ss#!)");
+            if (dados_bd == null) {
+                flag_geral = true;
+                break;
+            }
+
+            infor = dados_bd.split(";");
+            if (infor.length == 5) {
+                flagbd = a.connectarbd(infor[0], tipo_bd, infor[1], infor[2], infor[3], infor[4]);
+            }
+            if (!flagbd) {
+                flagbd_message = true;
+            }
+        } while (!flagbd);
+
+
+
+
+        boolean flagtab = false, flagtab_message = false;
+        do {
+
+            if (flag_geral) {
+                break;
+            }
+            //Indicar o nome da base de dados destino e o nome da tabela
+            if (flagtab_message) {
+                JOptionPane.showMessageDialog(null, "Tabela não existente na base de dados!");
+            }
+
+            nome_tabela = JOptionPane.showInputDialog(null, "Indique o nome da tabela:");
+            if (nome_tabela == null) {
+                flag_geral = true;
+                break;
+            }
+            flagtab = a.carregarInformacao(nome_tabela, uiController, infor[0], tipo_bd, infor[1], infor[2], infor[3], infor[4]);
+            if (!flagtab) {
+                flagtab_message = true;
+
+                if (nome_tabela == null) {
                     flag_geral = true;
                     break;
                 }
 
-                infor = dados_bd.split(";");
-                if (infor.length == 5) {
-                    flagbd = a.connectarbd(infor[0], tipo_bd, infor[1], infor[2], infor[3], infor[4]);
-                }
-                if (!flagbd) {
-                    flagbd_message = true;
-                }
-            } while (!flagbd);
-
-
-
-
-            boolean flagtab = false, flagtab_message = false;
-            do {
-
-                if (flag_geral) {
-                    break;
-                }
-                //Indicar o nome da base de dados destino e o nome da tabela
-                if (flagtab_message) {
-                    JOptionPane.showMessageDialog(null, "Tabela não existente na base de dados!");
-                }
-                
-                nome_tabela = JOptionPane.showInputDialog(null, "Indique o nome da tabela:");
-                 if(nome_tabela==null){
-                     flag_geral=true;
-                     break;
-                 }
-                flagtab = a.carregarInformacao(nome_tabela, uiController, infor[0] , tipo_bd, infor[1], infor[2], infor[3], infor[4]);
-                if (!flagtab) {
-                    flagtab_message = true;
-
-                    if (nome_tabela == null) {
-                        flag_geral = true;
-                        break;
-                    }
-
-                }
-            } while (!flagtab);
-            if (!flag_geral) {
-                //Aqui deve-se preencher a matriz 
-                
-                
-                JOptionPane.showMessageDialog(null, "Informação exportada com sucesso!");
             }
-       
+        } while (!flagtab);
+        if (!flag_geral) {
+            //Aqui deve-se preencher a matriz 
+
+
+            JOptionPane.showMessageDialog(null, "Informação exportada com sucesso!");
+        }
+
     }
 
     /**
@@ -377,12 +573,12 @@ public class BaseDadosSyncAction extends BaseAction {
             //Correr a thread criada
             Thread a = new Thread(new ThreadBD());
             a.start();
-           
-            
+
+
 
 
         } catch (Exception ex) {
-             ex.printStackTrace();
+            ex.printStackTrace();
             // para ja ignoramos a excepcao
         }
     }
